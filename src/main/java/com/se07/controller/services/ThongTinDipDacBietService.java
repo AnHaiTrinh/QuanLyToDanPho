10a6536161da76538af08434811b171ca9df1dbf
john <khoithanthanh02@gmail.com>
2023-02-07 00:14:43 +0700

    update ThongTinDipDacBietService

--- src/main/java/com/se07/controller/services/ThongTinDipDacBietService.java
+++ src/main/java/com/se07/controller/services/ThongTinDipDacBietService.java
@@ -142,6 +142,34 @@ public class ThongTinDipDacBietService {
         return list;
     }
 
+    public ObservableList<ThongTinDipDacBietDisplayModel> getAllThongTinDipDacBietByTenDipById(int idDip) {
+        ObservableList<ThongTinDipDacBietDisplayModel> list = FXCollections.observableArrayList();
+        Connection connection = ConnectionDatabase.getConnection();
+        String query = "select t.idNhap, t.maNhanKhau, n.hoTen, d.tenDip, d.nam, t.tinhTrang" +
+                " from thong_tin_dip_dac_biet t, nhan_khau n, dip_trao_thuong d " +
+                "where t.maNhanKhau = n.maNhanKhau and t.idDip = d.id" +
+                "and t.idDip='"+idDip+"'";
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+            while (rs.next()) {
+                ThongTinDipDacBietDisplayModel temp = new ThongTinDipDacBietDisplayModel(
+                        rs.getInt("idNhap"),
+                        rs.getString("maNhanKhau"),
+                        rs.getNString("hoTen"),
+                        rs.getNString("tenDip"),
+                        rs.getInt("nam"),
+                        rs.getNString("tinhTrang"));
+                list.add(temp);
+            }
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
     public ObservableList<ThongTinDipDacBietDisplayModel> getAllThongTinDipDacBietByNam(int nam) {
         ObservableList<ThongTinDipDacBietDisplayModel> list = FXCollections.observableArrayList();
         Connection connection = ConnectionDatabase.getConnection();
